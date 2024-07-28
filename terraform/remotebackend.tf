4b47630cddf4ce898c25f9fc426c76a46ee49e2b
Priyadharshini <pdharshini854@gmail.com>
2024-07-28 21:11:14 +0800

    Debug

--- terraform/remotebackend.tf
+++ terraform/remotebackend.tf
@@ -67,77 +67,17 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 resource "aws_dynamodb_table" "resume_statelock_table" {
   name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "event"
+  hash_key     = "LockID"
 
   attribute {
-    name = "event"
+    name = "LockID"
     type = "S"
   }
 
   tags = {
-    purpose     = "cloudresumechallenge"
-    Environment = "production"
+    purpose = "cloudresumechallenge"
   }
 }
 
 
-resource "aws_dynamodb_table_item" "event_test" {
-  table_name = aws_dynamodb_table.resume_statelock_table.name
-  hash_key   = aws_dynamodb_table.resume_statelock_table.hash_key
 
-  lifecycle {
-    ignore_changes = all
-  }
-
-  item = <<ITEM
-{
-  "event": {"S": "test"},
-  "visits":{"N":"0"}
-}
-ITEM
-}
-
-resource "aws_dynamodb_table_item" "event_prod" {
-  table_name = aws_dynamodb_table.resume_statelock_table.name
-  hash_key   = aws_dynamodb_table.resume_statelock_table.hash_key
-
-  lifecycle {
-    ignore_changes = all
-  }
-
-  item = <<ITEM
-{
-  "event": {"S": "prod"},
-  "visits":{"N":"0"}
-}
-ITEM
-}
-
-# Create a more specific IAM policy for DynamoDB access
-resource "aws_iam_policy" "dynamodb_access_policy" {
-  name        = "DynamoDBAccessPolicy"
-  description = "Policy to allow GetItem and PutItem on DynamoDB table"
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem"
-        ],
-        "Resource" : "arn:aws:dynamodb:${var.region}:${data.aws_caller_identity.current.account_id}:table/${var.table_name}"
-      }
-    ]
-  })
-}
-
-#Attaching AdministratorAccess policy 
-resource "aws_iam_user_policy_attachment" "dynamodb_user_policyattachment" {
-  user       = aws_iam_user.terraform_user_name.id
-  policy_arn = aws_iam_policy.dynamodb_access_policy.arn
-}
-
-
-# Data source to get current account ID
-data "aws_caller_identity" "current" {}
