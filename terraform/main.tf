68bea46466ca287b40c576acba08812341d64b57
Priyadharshini <pdharshini854@gmail.com>
2024-08-12 11:11:10 +0800

    Adding config files

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,13 +11,13 @@ provider "aws" {
   region = var.region
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "priya-resume-challenge-bucket-2"
-#     key            = "remote/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "resume-statelock-table-2"
+terraform {
+  backend "s3" {
+    bucket         = "priya-resume-challenge-bucket-2"
+    key            = "remote/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "resume-statelock-table-2"
 
-#   }
-# }
+  }
+}
 
